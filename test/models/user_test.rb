6bce1ea8168b1ab76dcee58d3789d65ce90d702b
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-15 23:00:29 +0900

    Add Password and Password Confirmation to User Test Setup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Daniel Park", email: "daniel@park.com")
+    @user = User.new(name: "Daniel Park", email: "daniel@park.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
