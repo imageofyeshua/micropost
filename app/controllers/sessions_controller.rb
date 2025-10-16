a0d49089c7912cad0b1b17f06ffaae72f7535943
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-17 00:13:24 +0900

    Current User and Logged In Methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].strip.downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:alert] = "Invalid email/password combination"
       render 'new', status: :unprocessable_entity
