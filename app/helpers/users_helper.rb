c9de106f87034c0e7477beb67488c39231d93144
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-16 11:43:08 +0900

    Gravatar Image Display Method for User

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
+require 'uri'
+
 module UsersHelper
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::SHA256::hexdigest(user.email.downcase)
+    default = "https://www.example.com/default.jpg"
+    size = 60
+    params = URI.encode_www_form('d' => default, 's' => size)
+    image_src = "https://gravatar.com/avatar/#{gravatar_id}?#{params}"
+    image_tag(image_src, alt: user.name, class: "gravatar")
+  end
 end
