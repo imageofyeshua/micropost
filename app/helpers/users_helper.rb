b4d906102a09d75c7e01d66c213855ed60ef12ab
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-17 09:49:55 +0900

    Disable Gravatar Method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,6 +5,8 @@ module UsersHelper
 
   # Displays Gravatar image if available, otherwise local default profile image
   def gravatar_for(email, size = 80)
+      image_tag("default-profile.png", alt: "#{email.strip.downcase}", class: "rounded-lg", size: "#{size}x#{size}")
+=begin
     gravatar_id = Digest::MD5.hexdigest(email.strip.downcase)
     gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=404"
 
@@ -17,7 +19,8 @@ module UsersHelper
     if response.code == "200"
       image_tag(gravatar_url, alt: "User Gravatar", class: "rounded-lg", size: "#{size}x#{size}")
     else
-      image_tag("default-profile.png", alt: "Default Profile", class: "rounded-lg", size: "#{size}x#{size}")
+      image_tag("default-profile.png", alt: "#{email.strip.downcase}", class: "rounded-lg", size: "#{size}x#{size}")
     end
+=end
   end
 end
