82263ae57dae4c98751a519448d5a272acdc53b5
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-18 21:15:44 +0900

    Authorization for Current User Only Edit and Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -38,6 +41,21 @@ class UsersController < ApplicationController
 
   def user_params
     params.expect(user: [:name, :email, :password, :password_confirmation])
+  end
+
+  # Before filters
+  # Confirms a logged-in user
+  def logged_in_user
+    unless logged_in?
+      flash[:alert] = "Please log in."
+      redirect_to login_url, status: :see_other
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 
 end
