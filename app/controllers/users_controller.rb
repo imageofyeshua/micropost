ee1f87c5d996827bdff4b1454345c67a16fafc43
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-18 19:35:39 +0900

    Successful User Edit Test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  private
-    
-    def user_params
-      params.expect(user: [:name, :email, :password, :password_confirmation])
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
     end
-  
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [:name, :email, :password, :password_confirmation])
+  end
+
 end
