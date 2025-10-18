0915a0026624b2da238e3a2541758b9c6292c84d
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-18 22:15:20 +0900

    Friendly Forwarding Method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,7 @@ class UsersController < ApplicationController
   # Confirms a logged-in user
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:alert] = "Please log in."
       redirect_to login_url, status: :see_other
     end
