64c96df978df52b8a601788a3ab0653f6e36f0a1
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-19 10:45:38 +0900

    Add User Name to Delete Flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,8 +43,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    user = User.find(params[:id])
+    user.destroy
+    flash[:success] = "User [#{user.name}] deleted"
     redirect_to users_url, status: :see_other
   end
 
