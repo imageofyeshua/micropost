6f4da96f2d3010e849ded0eee3eecc7bb6dfdd6f
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-19 09:50:21 +0900

    Add Admin to Users Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_17_045234) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_19_004928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_17_045234) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
